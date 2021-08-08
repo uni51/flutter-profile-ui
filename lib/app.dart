ae2ae85343db0e1abad7ebff269085b7825672f6
Koichi.Shimomura <unieye51@gmail.com>
2021-08-08 11:20:01 +0900

    62. Profile App part 4

--- lib/app.dart
+++ lib/app.dart
@@ -9,32 +9,54 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text("Profile UI"),
         ),
-        body: Column(
-          children: <Widget>[
-            Image.asset("images/love-of-mother.jpg", width: 200, height: 100),
-            ListTile(
-              title: Text("Lutfor Rahman"),
-              subtitle: Text("Sr. Web and Mobile Application Developer"),
-            ),
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.green.shade300, Colors.green.shade50])),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage("images/love-of-mother.jpg"),
+              ),
+              ListTile(
+                title: Center(
+                    child: Text(
+                  "Lutfor Rahman",
+                  style: TextStyle(fontSize: 20),
+                )),
+                subtitle: Center(
+                    child: Text(
+                  "Sr. Web and Mobile Application Developer",
+                  style: TextStyle(fontSize: 15),
+                )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  // ignore: deprecated_member_use
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("Message"),
+                  ),
 
-            // ignore: deprecated_member_use
-            RaisedButton(
-              onPressed: () {},
-              child: Text("Message"),
-            ),
-
-            // ignore: deprecated_member_use
-            RaisedButton(
-              onPressed: () {},
-              child: Text("Hire Me"),
-            ),
-
-            ListTile(
-              title: Text("About Me"),
-              subtitle: Text(
-                  "Over 8 years of Experienced Web and Mobile application developer and architect with a demonstrated history of working in the information technology and services industry. Skilled in .NET Framework (C#), ASP.NET MVC, Web API, Entity Framework, SQL Server, PHP, Laravel Framework, MySQL, Javascript, Angularjs, Angular, Java (Android), Swift (iOS), Dart & Flutter, Node & ExpressJS, Python & Django, AWS, Azure and Unit Testing using NUnit, PHPUnit and Jasmine."),
-            ),
-          ],
+                  // ignore: deprecated_member_use
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("Hire Me"),
+                  ),
+                ],
+              ),
+              ListTile(
+                title: Text("About Me", style: TextStyle(fontSize: 20)),
+                subtitle: Text(
+                    "Over 8 years of Experienced Web and Mobile application developer and architect with a demonstrated history of working in the information technology and services industry. Skilled in .NET Framework (C#), ASP.NET MVC, Web API, Entity Framework, SQL Server, PHP, Laravel Framework, MySQL, Javascript, Angularjs, Angular, Java (Android), Swift (iOS), Dart & Flutter, Node & ExpressJS, Python & Django, AWS, Azure and Unit Testing using NUnit, PHPUnit and Jasmine.",
+                    style: TextStyle(fontSize: 15)),
+              ),
+            ],
+          ),
         ),
       ),
     );
